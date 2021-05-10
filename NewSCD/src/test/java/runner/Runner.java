62c13e1e662bb38327b7ad099f7f0392ce8e2af8
manoj.mali <manoj.mali@sw12-OPVDI74.sp.com>
2021-05-10 14:37:15 +0530

    Manoj

--- NewSCD/src/test/java/runner/Runner.java
+++ NewSCD/src/test/java/runner/Runner.java
@@ -13,11 +13,12 @@ import io.cucumber.junit.CucumberOptions;
 	glue= {"stepdefinition"},
 	plugin= {"pretty", "html:target/TestReport.html"},
 		tags="@Smoketest1 or @Smoketest2 or @Smoketest7", 
+				//tags="@Smoketest1 or @Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5",
+				//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
+				
 	monochrome = true
 			)
 		
 	public class Runner {
-		//tags="@Smoketest1 or @Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5",
-		//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
 		
 }
