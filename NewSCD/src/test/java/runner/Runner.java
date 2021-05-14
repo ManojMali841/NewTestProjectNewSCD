f71d5d0f375177054c64c35a0ed2f8961e086194
manoj.mali <manoj.mali@sw12-OPVDI74.sp.com>
2021-05-14 12:49:36 +0530

    manoj

--- NewSCD/src/test/java/runner/Runner.java
+++ NewSCD/src/test/java/runner/Runner.java
@@ -9,16 +9,20 @@ import io.cucumber.junit.CucumberOptions;
 	@RunWith(Cucumber.class)
 	@CucumberOptions
 	(
-	features="C:\\Users/manoj.mali/git/repository2/NewSCD/src/test/java/feature/NewSCDLoginFeature.feature",
+	features="C:/Users/Deepak.Badgotya/eclipse-workspace/NewSCD/src/test/java/feature/NewSCDLoginFeature.feature",
 	glue= {"stepdefinition"},
-	plugin= {"pretty", "html:target/TestReport.html"},
-		//tags="@Smoketest1 or @Smoketest2 or @Smoketest7", 
-				tags="@Smoketest1 or @Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5",
-				//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
-				
+		plugin={"pretty", "html:target/cucumber-reports/cucumberhtml.html", "json:target/cucumber-reports/CucumberTestReport.json",
+				"junit:target/cucumber-reports/Cucumberjunit.xml"},
+	//plugin = ("json:target/cucumber-reports/CucumberTestReport.json"),
+			
+	tags="@Smoketest1 or @Smoketest2 or @Smoketest3 or @Smoketest4",
+			publish = true,
+			//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
+				//tags="@Smoketest1 or @Smoketest2 or @Smoketest7", 
+			//@Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5
 	monochrome = true
 			)
 		
 	public class Runner {
-		
+
 }
